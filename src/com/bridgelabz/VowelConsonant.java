23a9f14a820a58ab0f351c8b2b73f4a352068b4d
vinayakgarole <vinayakgarole10@gmail.com>
2022-10-20 15:07:14 +0530

    added: Check Vowel or Consonant using switch case

---
+++ src/com/bridgelabz/VowelConsonant.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an alphabet: ");
+        char c = sc.next().charAt(0);
+
+        switch (c) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println(c + " is a vowel.");
+                break;
+            default:
+                System.out.println(c + " is a consonant.");
+        }
+    }
+}
