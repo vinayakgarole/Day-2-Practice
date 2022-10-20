91360d6cdfae760244089da7342eb469d536cec4
vinayakgarole <vinayakgarole10@gmail.com>
2022-10-20 18:08:09 +0530

    added: Calculating distance using command line arguments

---
+++ src/com/bridgelabz/Distance.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Distance {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the value of X: ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        System.out.println("Enter the value of Y: ");
+        int y = sc.nextInt();
+
+        double d = Math.sqrt(x * x + y * y);
+        System.out.println("The total distance is: " + d);
+    }
+}
